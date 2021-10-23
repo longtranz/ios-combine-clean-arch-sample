9b5b15877dc90915a9c88b83f45382bc512afe4a
longtranz <tranminhlong88@gmail.com>
2021-10-24 06:55:12 +0700

    Create Project

---
+++ CombineCleanArchitectureTests/CombineCleanArchitectureTests.swift
@@ -0,0 +1,33 @@
+//
+//  CombineCleanArchitectureTests.swift
+//  CombineCleanArchitectureTests
+//
+//  Created by DucNT65.FIN on 10/24/21.
+//
+
+import XCTest
+@testable import CombineCleanArchitecture
+
+class CombineCleanArchitectureTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
